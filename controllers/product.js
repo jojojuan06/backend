95cd7e9faf325ddcac813de73baa3d6e9069cc09
P2-openclassroom <jonh_06@live.fr>
2022-03-14 14:02:05 +0100

    add product controller

---
+++ controllers/product.js
@@ -0,0 +1,17 @@
+//importer de model thing crée , pouvoir utiliser notre nouveau modèle Mongoose dans l'application, nous devons l'importer dans le fichier
+const Thing = require('../models/product');
+
+
+//---------controller------------- (contenue des routes)
+
+
+
+// POST
+
+// PUT 
+
+// DELETE
+
+// GET
+
+//-----------
