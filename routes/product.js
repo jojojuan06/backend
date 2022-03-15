ec6a5da4afc79e71ad5943ef44bd790bdbce47d9
P2-openclassroom <jonh_06@live.fr>
2022-03-15 11:24:09 +0100

    creation de route

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,16 @@
 //--- aplication require importer--------
 const express = require('express');//importer package express
+const productControllers = require('../controllers/product'); // importation du controller stuff
 
 // Creation d'un routeur
 const router = express.Router(); // avec la methode routeur d'expresse (ex remplace app.get par router.get 
 
 //---------  Routes  ----------- CRUD methode
-router.post('/', productControllers.createThing)
-router.put('/:id', productControllers.modifyThing);
-router.delete('/:id',  productControllers.deleteThing);
-router.get('/:id', productControllers.getOneThing);
-router.get('/',  productControllers.getAllThing);
+router.post('/', productControllers.createProduct)
+router.put('/:id', productControllers.modifyProduct);
+router.delete('/:id',  productControllers.deleteProduct);
+router.get('/:id', productControllers.getOneProduct);
+router.get('/',  productControllers.getAllProduct);
 
 //-------------
 
